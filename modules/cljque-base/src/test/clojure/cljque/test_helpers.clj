e40a0b3be4e98067c86b2161e22d1ff030681c6f
Stuart Sierra <mail@stuartsierra.com>
2010-11-07 11:01:41 -0500

    Rename cljque-api to cljque-base; fix tests

--- modules/cljque-base/src/test/clojure/cljque/test_helpers.clj
+++ modules/cljque-base/src/test/clojure/cljque/test_helpers.clj
@@ -10,14 +10,14 @@
 (defn send-message-test [listener-factory sender-factory]
   (let [message (random-string)
 	received (atom [])
-	receiver (fn [msg] (swap! received conj msg))]
+	receiver (fn [_ _ msg] (swap! received conj msg))]
     (do-it "can send and receive a message"
       (let [listener (listener-factory)]
-	(listen! listener receiver)
+	(subscribe listener :foo receiver)
 	(Thread/sleep 100)
 	(send! (sender-factory) message)
 	(Thread/sleep 100)
-	(stop! listener)
+	(unsubscribe listener :foo)
 	(expect (= @received [message]))))))
 
 
