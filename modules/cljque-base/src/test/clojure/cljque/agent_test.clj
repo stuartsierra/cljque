baedfab585ec342d0c4c5d65de2167d58189da89
Stuart Sierra <mail@stuartsierra.com>
2010-11-24 18:38:47 -0500

    Fix agent_test.clj

--- modules/cljque-base/src/test/clojure/cljque/agent_test.clj
+++ modules/cljque-base/src/test/clojure/cljque/agent_test.clj
@@ -1,17 +1,18 @@
-;; (ns cljque.agent-test
-;;   (:use cljque.api
-;; 	cljque.local
-;; 	cljque.combinators
-;; 	[lazytest.describe :only (describe do-it for-any)]
-;; 	[lazytest.expect :only (expect)]
-;; 	[lazytest.random :only (list-of string-of pick alphanumeric integer)]))
+(ns cljque.agent-test
+  (:use cljque.api
+	cljque.local
+	cljque.combinators
+	[lazytest.describe :only (describe do-it for-any)]
+	[lazytest.expect :only (expect)]
+	[lazytest.random :only (list-of string-of pick alphanumeric integer)]))
 
-;; (describe "Agents"
-;;   (for-any [nums (list-of (integer))]
-;; 	   (do-it "can accept send!"
-;; 	     (let [t (local ((string-of (pick alphanumeric))))
-;; 		   a (agent t)
-;; 		   s (seq-observable t)]
-;; 	       (doseq [n nums] (send! a n))
-;; 	       (await a)
-;; 	       (expect (= nums (take 1 s)))))))
+(describe "Agents"
+  (do-it "can accept send!"
+    (let [t (local ((string-of (pick alphanumeric))))
+	  a (agent t)
+	  state (atom nil)
+	  observer (fn [_ event] (reset! state event))]
+      (subscribe t observer)
+      (send! a 5)
+      (Thread/sleep 500)
+      (expect (= 5 @state)))))
