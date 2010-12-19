e3fd441d29ea7b8dcf5c73cc7019199f3888174b
Stuart Sierra <mail@stuartsierra.com>
2010-12-18 19:11:13 -0500

    ObservableNioServer record type

--- modules/cljque-netty/src/main/clojure/cljque/netty/events.clj
+++ modules/cljque-netty/src/main/clojure/cljque/netty/events.clj
@@ -4,29 +4,60 @@
 
 (import-netty)
 
-(defn observed-event [observable subscribers-map event]
-  (if (instance? event ExceptionEvent)
-    (doseq [subscriber (vals subscribers-map)]
-      (error subscriber observable (.getCause event)))
-    (doseq [subscriber (vals subscribers-map)]
-      (event subscriber observable event))))
+;;; Observables and Netty channel events
 
-(defn observable-server [server-state]
-  (let [{:keys [bootstrap]} server-state
-	old-factory (.getPipelineFactory bootstrap)
+(defn observed-event [observable subscribers-map netty-event]
+  (if (instance? ExceptionEvent netty-event)
+    (doseq [subscriber (vals subscribers-map)]
+      (error subscriber observable (.getCause netty-event)))
+    (doseq [subscriber (vals subscribers-map)]
+      (event subscriber observable netty-event))))
+
+(defn add-observable-handler [pipeline observable subscribers]
+  (add-to-pipeline
+   pipeline
+   "observable" (channel-upstream-handler
+		 (fn [context event]
+		   (observed-event observable
+				   @subscribers
+				   event)))))
+
+;;; Observable servers
+
+(defrecord ObservableNioServer
+  [bootstrap channel-group server-channel subscribers]
+  Observable
+  (subscribe [this observer]
+	     (let [key (Object.)]
+	       (swap! subscribers assoc key observer)
+	       (fn [] (swap! subscribers dissoc key)))))
+
+(defn create-observable-nio-server [port pipeline-factory-fn]
+  (let [bootstrap (nio-server-bootstrap)
+	channel-group (DefaultChannelGroup.)
 	subscribers (atom {})
-	observable (reify Observable
-			  (subscribe [this observer]
-				     (let [key (Object.)]
-				       (swap! subscribers assoc key observer)
-				       (fn [] (swap! subscribers dissoc key)))))]
-    (.setPipelineFactory bootstrap
-			 (channel-pipeline-factory
-			  #(add-to-pipeline
-			    (.getPipeline old-factory)
-			    "observable" (channel-upstream-handler
-					  (fn [context event]
-					    (observed-event observable
-							    @subscribers
-							    event))))))
-    observable))
+	observable (ObservableNioServer. bootstrap channel-group nil
+					 subscribers)]
+    (set-channel-pipeline-factory
+     bootstrap
+     #(-> (pipeline-factory-fn)
+	  (add-channel-group-handler channel-group)
+	  (add-observable-handler observable subscribers)))
+    (let [server-channel (bind bootstrap port)]
+      (.add channel-group server-channel)
+      (assoc observable :server-channel server-channel))))
+
+;;; MessageTarget channels
+
+(extend-protocol MessageTarget
+  Channel
+  (send! [target message] (Channels/write target message)))
+
+;;; Observable futures
+
+(extend-protocol Observable
+  ChannelFuture
+  (subscribe [channel-future observer]
+	     (let [listener (channel-future-listener #(done observer %))]
+	       (.addListener channel-future listener)
+	       (fn [] (.removeListener channel-future listener)))))
