b39ff0f8358e67758d88d16724b4a2917514bafc
Stuart Sierra <mail@stuartsierra.com>
2010-12-26 12:40:51 -0500

    Separate client and server HTTP pipelines

--- modules/cljque-netty/src/main/clojure/cljque/netty/http.clj
+++ modules/cljque-netty/src/main/clojure/cljque/netty/http.clj
@@ -3,17 +3,26 @@
 
 (import-netty)
 
-(defn default-http-pipeline
-  "Returns a simple ChannelPipeline which encodes/decodes HTTP
-  requests and responses. Automatically aggregates chunked HTTP
-  requests, which may have a maximum size of 10 MB. Compression is
-  handled automatically."
+(defn http-server-pipeline
+  "Returns a simple ChannelPipeline for use in an HTTP
+  server. Encodes/decodes HTTP requests and responses. Automatically
+  aggregates chunked HTTP requests, which may have a maximum size of
+  10 MB. Compression is handled automatically."
   []
   (pipeline
-   "http-decode" (HttpRequestDecoder.)
-   "http-decompress" (HttpContentDecompressor.)
+   "http-codec" (HttpServerCodec.)
    "http-aggregate" (HttpChunkAggregator. 10485760)
-   "http-encode" (HttpResponseEncoder.)
    "http-compress" (HttpContentCompressor.)
    "netty-log" (LoggingHandler.)))
 
+(defn http-client-pipeline
+  "Returns a simple ChannelPipeline for use in an HTTP
+  client. Encodes/decodes HTTP requests and responses. Automatically
+  aggregates chunked HTTP responses, which may have a maximum size of
+  10 MB. Compression is handled automatically."
+  []
+  (pipeline
+   "http-codec" (HttpClientCodec.)
+   "http-decompress" (HttpContentDecompressor.)
+   "http-aggregate" (HttpChunkAggregator. 10485760)
+   "netty-log" (LoggingHandler.)))
