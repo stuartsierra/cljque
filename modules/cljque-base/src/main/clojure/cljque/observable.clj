d99a5c746db0e112659b927025dd8d0a2659af18
Stuart Sierra <mail@stuartsierra.com>
2011-02-20 14:24:00 -0500

    Remove source from message sends, add error printing

--- modules/cljque-base/src/main/clojure/cljque/observable.clj
+++ modules/cljque-base/src/main/clojure/cljque/observable.clj
@@ -6,26 +6,35 @@
 (defprotocol Observable
   (observe [this agnt on-message on-done]
     "When this Observable generates a message,
-    calls (send agnt on-message this message)
+    calls (send agnt on-message message)
 
     When this Observable is finished generating messages,
-    calls (send agnt on-done this)
+    calls (send agnt on-done)
 
     Returns a no-arg function that cancels the subscription."))
+
+;;; Error handling
+
+(defn make-agent [state]
+  (agent state
+         :error-handler (fn [agnt err]
+                          (.println *err* (str "Error on " (pr-str agnt)))
+                          (.printStackTrace err *err*))
+         :error-mode :fail))
 
 ;;; Actions without state
 
 (defn nop [state & args] state)
 
 (defn static-receiver [f]
-  (fn [state source message] (f message) state))
+  (fn [state message] (f message) state))
 
 ;;; Subscription entry point
 
 (defn subscribe
   "Subscribes to messages."
   ([observable f]
-     (observe observable (agent nil) (static-receiver f) nop))
+     (observe observable (make-agent nil) (static-receiver f) nop))
   ([observable agnt on-message]
      (observe observable agnt on-message nop))
   ([observable agnt on-message on-done]
@@ -39,7 +48,7 @@
       (let [key (Object.)]
         (add-watch iref key
                    (fn [_ _ old-value new-value]
-                     (send agnt on-message this new-value)))
+                     (send agnt on-message new-value)))
         (fn [] (remove-watch iref key))))))
 
 ;;; Observables and Sequences
@@ -53,24 +62,24 @@
             (loop [xs s]
               (when @continue
                 (if-let [x (clojure.core/first xs)]
-                  (do (send agnt on-message this x)
+                  (do (send agnt on-message x)
                       (recur (next xs)))
-                  (send agnt on-done this))))
+                  (send agnt on-done))))
             (catch Throwable t
               (send agnt (fn [state] (throw t))))))
         (fn [] (reset! continue false))))))
 
 (defn seq-observable [observable]
   (let [q (java.util.concurrent.LinkedBlockingQueue.)
-        consumer (fn this []
+        consumer (fn thisfn []
                    (lazy-seq
                     (let [x (.take q)]
                       (when-not (= x ::done)
-                        (cons x (this))))))]
+                        (cons x (thisfn))))))]
     (subscribe observable
-               (agent nil)
-               (fn [state src message] (.put q message))
-               (fn [state src] (.put q ::done)))
+               (make-agent nil)
+               (fn [state message] (.put q message))
+               (fn [state] (.put q ::done)))
     (consumer)))
 
 ;;; One-time event generators
@@ -78,14 +87,14 @@
 (defn value [x]
   (reify Observable
     (observe [this agnt on-message on-done]
-      (send agnt on-message this x)
-      (send agnt on-done this)
+      (send agnt on-message x)
+      (send agnt on-done)
       (constantly nil))))
 
 (defn never []
   (reify Observable
     (observe [this agnt on-message on-done]
-      (send agnt on-done this)
+      (send agnt on-done)
       (constantly nil))))
 
 ;;; "Push" sequence API
@@ -94,11 +103,11 @@
   ([]
      (reify Observable
        (observe [this agnt on-message on-done]
-         (let [a (agent 0)]
+         (let [a (make-agent 0)]
            (send a
                  (fn thisfn [state]
                    (when state
-                     (send agnt on-message this state)
+                     (send agnt on-message state)
                      (send *agent* thisfn)
                      (inc state))))
            (fn [] (send a (constantly false)))))))
@@ -109,56 +118,56 @@
   ([start end step]
      (reify Observable
        (observe [this agnt on-message on-done]
-         (let [a (agent start)]
+         (let [a (make-agent start)]
            (send a
                  (fn thisfn [state]
                    (if (< state end)
-                     (do (send agnt on-message this state)
+                     (do (send agnt on-message state)
                          (send *agent* thisfn)
                          (+ state step))
-                     (send agnt on-done this))))
+                     (send agnt on-done))))
            (fn [] (send a (constantly end))))))))
 
 (defn map [f source]
   (reify Observable
     (observe [this agnt on-message on-done]
       (subscribe source agnt
-                 (fn [state src message]
-                   (on-message state src (f message)))))))
+                 (fn [state message]
+                   (on-message state (f message)))))))
 
 (defn filter [pred source]
   (reify Observable
     (observe [this agnt on-message on-done]
       (subscribe source agnt
-                 (fn [state src message]
+                 (fn [state message]
                    (when (pred message)
-                     (on-message state src message)))))))
+                     (on-message state message)))))))
 
 (defn take [n source]
   (reify Observable
     (observe [this agnt on-message on-done]
       (let [unsub (promise)]
         (deliver unsub
-                 (subscribe source (agent n)
-                            (fn [state src message]
+                 (subscribe source (make-agent n)
+                            (fn [state message]
                               (if (zero? state)
                                 (do (@unsub)
-                                    (send agnt on-done this))
-                                (send agnt on-message this message))
+                                    (send agnt on-done))
+                                (send agnt on-message message))
                               (dec state))
-                            (fn [_ _] (send agnt on-done this))))
+                            (fn [state] (send agnt on-done) nil)))
         (fn [] (@unsub))))))
 
 (defn drop [n source]
   (reify Observable
     (observe [this agnt on-message on-done]
-      (let [unsub (subscribe source (agent n)
-                            (fn [state src message]
+      (let [unsub (subscribe source (make-agent n)
+                            (fn [state message]
                               (if (zero? state)
-                                (do (send agnt on-message this message)
+                                (do (send agnt on-message message)
                                     state)
                                 (dec state)))
-                            (fn [_ _] (send agnt on-done this) nil))]
+                            (fn [state] (send agnt on-done) nil))]
         (fn [] (unsub))))))
 
 (defn first [source]
@@ -170,28 +179,28 @@
 (defn concat [& sources]
   (reify Observable
     (observe [this agnt on-message on-done]
-      (let [a (agent (clojure.core/next sources))]
-        (letfn [(on-msg [state src message]
-                  (send agnt on-message this message)
+      (let [a (make-agent (clojure.core/next sources))]
+        (letfn [(on-msg [state message]
+                  (send agnt on-message message)
                   state)
-                (on-dn [state src]
+                (on-dn [state]
                   (if state
                     (do (subscribe (clojure.core/first state) a on-msg on-dn)
                         (clojure.core/next state))
-                    (send agnt on-done this)))]
+                    (send agnt on-done)))]
           (subscribe (clojure.core/first sources) a on-msg on-dn))))))
 
 (defn distinct [source]
   (reify Observable
     (observe [this agnt on-message on-done]
-      (subscribe source (agent #{})
-                 (fn [state src message]
+      (subscribe source (make-agent #{})
+                 (fn [state message]
                    (if (contains? state message)
                      state
-                     (do (send agnt on-message src message)
+                     (do (send agnt on-message message)
                          (conj state message))))
-                 (fn [state src]
-                   (send agnt on-done src)
+                 (fn [state]
+                   (send agnt on-done)
                    nil)))))
 
 (defn cycle [coll]
@@ -199,10 +208,10 @@
     (observe [this agnt on-message on-done]
       (let [v (vec coll)
             c (count coll)
-            a (agent 0)]
+            a (make-agent 0)]
         (send a (fn thisfn [state]
                   (when state
-                   (send agnt on-message this (nth v state))
+                   (send agnt on-message (nth v state))
                    (send *agent* thisfn)
                    (mod (inc state) c))))
         (fn [] (send a (constantly false)))))))
@@ -212,12 +221,12 @@
 (defn transitions [source]
   (reify Observable
     (observe [this agnt on-message on-done]
-      (subscribe source (agent ::unset)
-                 (fn [state src message]
-                   (send agnt on-message src [state message])
+      (subscribe source (make-agent ::unset)
+                 (fn [state message]
+                   (send agnt on-message [state message])
                    message)
-                 (fn [state src]
-                   (send agnt on-done this)
+                 (fn [state]
+                   (send agnt on-done)
                    nil)))))
 
 (defn changes [source]
@@ -231,7 +240,7 @@
 (defn gather [& sources]
   (reify Observable
     (observe [this agnt on-message on-done]
-      (let [results (agent (vec (repeat (count sources) ::unset)))
+      (let [results (make-agent (vec (repeat (count sources) ::unset)))
             unsubs (promise)]
         (deliver unsubs
                  (doall
@@ -239,15 +248,15 @@
                    (fn [i source]
                      (subscribe
                       (take 1 source) results
-                      (fn [state src message]
+                      (fn [state message]
                         (let [new-state (assoc state i message)]
                           (when (every? #(not= ::unset %) new-state)
-                            (send agnt on-message this new-state))
+                            (send agnt on-message new-state))
                           new-state))
-                      (fn [state src]
+                      (fn [state]
                         (when (= ::unset (nth state i))
                           (doseq [u @unsubs] (u))
-                          (send agnt on-done this))
+                          (send agnt on-done))
                         state)))
                    sources)))
         (fn [] (doseq [u @unsubs] (u)))))))
@@ -255,19 +264,19 @@
 (defn merge [& sources]
   (reify Observable
     (observe [this agnt on-message on-done]
-      (let [counter (agent (count sources))
+      (let [counter (make-agent (count sources))
             unsubs (doall
                     (map-indexed
                      (fn [i source]
                        (subscribe
                         source counter
-                        (fn [state src message]
-                          (send agnt on-message this message)
+                        (fn [state message]
+                          (send agnt on-message message)
                           state)
-                        (fn [state src]
+                        (fn [state]
                           (let [new-state (dec state)]
                             (when (zero? new-state)
-                              (send agnt on-done this))
+                              (send agnt on-done))
                             new-state))))
                      sources))]
         (fn [] (doseq [u unsubs] (u)))))))
@@ -277,13 +286,13 @@
 (defn delay [d units message]
   (reify Observable
     (observe [this agnt on-message on-done]
-      (let [fut (delay-send d units agnt on-message this message)]
+      (let [fut (delay-send d units agnt on-message message)]
         (fn [] (.cancel fut false))))))
 
 (defn periodic [init d units message]
   (reify Observable
     (observe [this agnt on-message on-done]
-      (let [fut (periodic-send init d units agnt on-message this message)]
+      (let [fut (periodic-send init d units agnt on-message message)]
         (fn [] (.cancel fut false))))))
 
 (defn timeout [d units source]
