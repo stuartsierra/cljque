21bb759aa9e2a23e0e2e277bc4094885d2c08904
Stuart Sierra <mail@stuartsierra.com>
2011-02-20 14:24:30 -0500

    Use bound-fn* in scheduler calls

--- modules/cljque-base/src/main/clojure/cljque/schedule.clj
+++ modules/cljque-base/src/main/clojure/cljque/schedule.clj
@@ -12,12 +12,15 @@
    :microseconds TimeUnit/MICROSECONDS
    :nanoseconds TimeUnit/NANOSECONDS})
 
+;; Instead of bound-fn*, I would like to use the (private)
+;; clojure.core/binding-conveyor-fn
+
 (defn delay-send [d units agnt action & args]
   (.schedule (force executor)
-             #(apply send agnt action args)
+             (bound-fn* #(apply send agnt action args))
              d (time-unit units)))
 
 (defn periodic-send [init d units agnt action & args]
   (.scheduleAtFixedRate (force executor)
-                        #(apply send agnt action args)
+                        (bound-fn* #(apply send agnt action args))
                         init d (time-unit units)))
