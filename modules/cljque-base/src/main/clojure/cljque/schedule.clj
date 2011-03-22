1eaa0376d257a7e1fa5feb4769f61b513cb41c14
Stuart Sierra <mail@stuartsierra.com>
2011-03-21 21:40:55 -0400

    Separate delay/periodic function calls from agent actions in cljque.schedule

--- modules/cljque-base/src/main/clojure/cljque/schedule.clj
+++ modules/cljque-base/src/main/clojure/cljque/schedule.clj
@@ -15,14 +15,30 @@
 ;; Instead of bound-fn*, I would like to use the (private)
 ;; clojure.core/binding-conveyor-fn
 
-(defn delay-send [d units agnt action & args]
-  (let [fut (.schedule (force executor)
-                       (bound-fn* #(apply send agnt action args))
+(defn delay-call
+  "Invokes f after d units of time. Returns a no-argument function
+  which cancels the invocation if it has not already occurred."
+  [d units f]
+  (let [fut (.schedule (force executor) f
                        d (time-unit units))]
     (fn [] (.cancel fut false))))
 
-(defn periodic-send [init d units agnt action & args]
-  (let [fut (.scheduleAtFixedRate (force executor)
-                                  (bound-fn* #(apply send agnt action args))
+(defn delay-send
+  "Sends action to agnt after d units of time, with extra arguments."
+  [d units agnt action & args]
+  (delay-call d units (bound-fn* #(apply send agnt action args))))
+
+(defn periodic-call
+  "Invokes f every d units of time, after an initial delay of init
+  units.  Returns a no-argument function which cancels the scheduled
+  event."
+  [init d units f]
+  (let [fut (.scheduleAtFixedRate (force executor) f                                  
                                   init d (time-unit units))]
     (fn [] (.cancel fut false))))
+
+(defn periodic-send
+  "Sends action to agnt every d units of time, after an initial delay
+  of init units."
+  [init d units agnt action & args]
+  (periodic-call init d units (bound-fn* #(apply send agnt action args))))
