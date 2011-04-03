702b0a461fe97285a5187d49094aa0192a26772c
Stuart Sierra <mail@stuartsierra.com>
2011-04-03 11:26:32 -0400

    Add cljque.events, an alternative approach

---
+++ modules/cljque-base/src/main/clojure/cljque/events.clj
@@ -0,0 +1,111 @@
+(ns cljque.events
+  (:refer-clojure :exclude (map filter take)))
+
+(defprotocol Observable
+  (register [this f]))
+
+(defprotocol Observation
+  (value [this])
+  (observe [this]))
+
+(def NilCloseable
+  (reify java.io.Closeable
+    (close [this] nil)))
+
+(extend-type nil
+  Observation
+  (value [this] nil)
+  (observe [this] nil)
+  Observable
+  (register [this f]
+    (future (f nil))
+    NilCloseable))
+
+(deftype ObservedValue [x]
+  Observation
+  (value [this] x)
+  (observe [this] this))
+
+(deftype ObservedThrowable [t]
+  Observation
+  (value [this] (throw t))
+  (observe [this] this))
+
+(defn ^:dynamic relay [x] nil)
+
+(defn stop [] (relay nil))
+
+(defn push [x] (relay (ObservedValue. x)))
+
+(defmacro lens [event-source & body]
+  `(reify Observable
+     (register [this g#]
+       (register ~(second event-source)
+                 (fn [~(first event-source)]
+                   (binding [relay g#]
+                     (try ~@body
+                          (catch Throwable t#
+                            (relay (ObservedThrowable. t#))))))))))
+
+(defn observable-seq [s]
+  (reify Observable
+    (register [this f]
+      (let [open? (atom true)]
+        (future
+          (try 
+            (loop [s s]
+              (when (and (seq s) @open?)
+                (f (ObservedValue. (first s)))
+                (recur (rest s))))
+            (f nil)
+            (catch Throwable t
+              (f (ObservedThrowable. t)))))
+        (reify java.io.Closeable
+          (close [this]
+            (reset! open? false)))))))
+
+(defn seq-observable [src]
+  (let [q (java.util.concurrent.LinkedBlockingQueue.)
+        NIL (Object.)  ; can't put nil in a Queue
+        consumer (fn thisfn []
+                   (lazy-seq
+                    (let [event (.take q)]
+                      (when (not= NIL event)
+                        (cons (value event) (thisfn))))))]
+    (register src (fn [event] (.put q (if (nil? event) NIL event))))
+    (consumer)))
+
+(comment
+  ;; This is what `map` looks like without `lens`,
+  ;; not including error-handling
+  (defn map [f src]
+    (reify Observable
+      (register [this g]
+        (register src
+                  (fn [event]
+                    (if (observe event)
+                      (g (ObservedValue. (f (value event))))
+                      (g event))))))))
+
+(defn map [f src]
+  (lens [event src]
+        (if (observe event)
+          (push (f (value event)))
+          (stop))))
+
+(defn filter [f src]
+  (lens [event src]
+        (if (observe event)
+          (when (f (value event))
+            (relay event))
+          (stop))))
+
+(defn take [n src]
+  (let [a (atom n)]
+    (lens [event src]
+          (if (observe event)
+            (let [x (swap! a #(if (neg? %) % (dec %)))]
+              (when (not (neg? x))
+                (relay event))
+              (when (zero? x)
+                (stop)))))))
