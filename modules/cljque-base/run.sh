fcbfa7c3bd5653cb1716b592e0a72a13d7dc37ef
Stuart Sierra <mail@stuartsierra.com>
2011-05-28 11:10:46 -0400

    Replace module structure with latest Notify interface draft

--- modules/cljque-base/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd `dirname $0`
-
-exec java -cp src/main/clojure:/Users/stuart/.m2/repository/org/clojure/clojure/1.3.0-master-SNAPSHOT/clojure-1.3.0-master-SNAPSHOT.jar:/Users/stuart/.m2/repository/swank-clojure//swank-clojure/1.3.0-SNAPSHOT/swank-clojure-1.3.0-SNAPSHOT.jar clojure.main
