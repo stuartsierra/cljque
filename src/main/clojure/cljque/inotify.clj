e29714dbf9ecf247d0017b8dcf7246ed74b8e841
Stuart Sierra <mail@stuartsierra.com>
2011-06-13 21:03:19 +0200

    Crude versions of map and filter

--- src/main/clojure/cljque/inotify.clj
+++ src/main/clojure/cljque/inotify.clj
@@ -160,3 +160,23 @@
 
 (defmethod clojure.core/print-method Pump [x writer]
   (.write writer "#<Pump>"))
+
+(defn a-map [f s]
+  (let [p (proseq)]
+    (register s (reify Notify
+                  (notify [this that]
+                    (supply p (when-let [c (seq that)]
+                                (cons (f (first c))
+                                      (a-map f (rest c))))))))
+    p))
+
+(defn a-filter [f s]
+  (let [p (proseq)]
+    (register s (reify Notify
+                  (notify [this that]
+                    (supply p (when-let [c (seq that)]
+                                (if (f (first c))
+                                  (cons (first c)
+                                        (a-filter f (rest c)))
+                                  (a-filter f (rest c))))))))
+    p))
