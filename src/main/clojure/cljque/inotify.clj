6e9564f6ae138260f619c46df01c49509aa06e6c
Stuart Sierra <mail@stuartsierra.com>
2011-06-13 20:48:36 +0200

    Remove Terminate protocol and do not re-register return value of notify

--- src/main/clojure/cljque/inotify.clj
+++ src/main/clojure/cljque/inotify.clj
@@ -3,9 +3,6 @@
 (defprotocol Supply
   (supply [ipending value]))
 
-(defprotocol Terminate
-  (terminate [ipending]))
-
 (defprotocol Notify
   (notify [observer ipending]))
 
@@ -79,52 +76,37 @@
   (register [this observer]
     (when-not (dosync (when @q
                         (alter q conj observer)))
-      (when-let [n (notify observer this)]
-        (register (rest this) n))
-      this))
+      (notify observer this))
+    this)
   Supply
   (supply [this x]
-    (if-let [watchers (dosync (when @q
-                                (ref-set v x)
-                                (let [dq @q]
-                                  (ref-set q nil)
-                                  dq)))]
-      (do (.countDown latch)
-          (doseq [w watchers]
-            (when-let [n (notify w this)]
-              (register (rest this) n))))
-      (supply (rest this) x))
+    (when-let [watchers (dosync (when @q
+                                  (ref-set v x)
+                                  (let [dq @q]
+                                    (ref-set q nil)
+                                    dq)))]
+      (.countDown latch)
+      (doseq [w watchers]
+        (notify w this)))
     this)
-  Terminate
-  (terminate [this]
-    (if-let [watchers (dosync (when @q
-                                (ref-set v nil)
-                                (let [dq @q]
-                                  (ref-set q nil)
-                                  dq)))]
-      (do (.countDown latch)
-          (doseq [w watchers]
-            (notify w nil)))
-      (terminate (rest this))))
   clojure.lang.IPending
   (isRealized [this]
     (zero? (.getCount latch)))
-  clojure.lang.ISeq
-  (first [this]
-    (.await latch)
-    (first @v))
-  (more [this]
-    (.await latch)
-    (rest @v))
-  (next [this]
-    (seq (rest this)))
+  clojure.lang.Seqable
   (seq [this]
     (.await latch)
     (seq @v))
+  clojure.lang.ISeq
+  (first [this]
+    (first (seq this)))
+  (more [this]
+    (rest (seq this)))
+  (next [this]
+    (seq (rest this)))
   (count [this]
-    (clojure.core/count this))
+    (count (seq this)))
   (cons [this o]
-    (clojure.core/cons o this))
+    (clojure.lang.Cons. o this))
   (empty [this]
     (proseq))
   (equiv [this that]
@@ -170,29 +152,5 @@
      {:pre [(instance? PromisedSeq s)]}
      (Pump. (atom s))))
 
-(defn siphon "Iterate over an PromisedSeq, return another PromisedSeq."
-  [f s] nil)
-
-(defn sink "Reduce over an PromisedSeq, return an ActivePromise."
-  [f init s] nil)
-
-(comment
- (deftype NextReceiver [f])
-
- (defn next-receiver [f]
-   (NextReceiver. f))
-
- (defn sink-notifier [sink f]
-   (reify Notify
-     (notify [this that]
-       (loop [xs (f that)]
-         (when-first [x xs]
-           (if (instance? NextReceiver x)
-             (sink-notifier sink (.f x))
-             (do (supply target x)
-                 (recur (rest xs)))))))))
-
- (defn postpone-seq [source f]
-   (let [target (proseq)]
-     (register source (target-seq-notifier target f))
-     target)))
+(defmethod clojure.core/print-method Pump [x writer]
+  (.write writer "#<Pump>"))
