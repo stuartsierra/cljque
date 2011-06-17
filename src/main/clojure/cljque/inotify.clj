11b987b332b935bfe64b86b00235fbc8ec27bedd
Stuart Sierra <mail@stuartsierra.com>
2011-06-16 23:32:24 -0400

    Down to a single protocol, Register

--- src/main/clojure/cljque/inotify.clj
+++ src/main/clojure/cljque/inotify.clj
@@ -1,31 +1,26 @@
 (ns cljque.inotify)
 
-(defprotocol Supply
-  (supply [ipending value]))
-
 (defprotocol Register
-  (register [ipending f]))
+  (register [this f]
+    "Register callback f to be invoked on the value of this when it
+    becomes available."))
 
-(defn active-promise []
+(defn notifier
+  "Returns a promise on which callbacks can be registered."
+  []
   (let [latch (java.util.concurrent.CountDownLatch. 1)
-        q (ref [])
-        v (ref nil)]
+        q (ref []) ;; make this a Java Queue?
+        v (ref nil)] ;; make this an atom, use the Queue as a marker?
     (reify
-      clojure.lang.IPending
-      (isRealized [this]
-        (zero? (.getCount latch)))
-      clojure.lang.IDeref
-      (deref [this]
-        (.await latch)
-        @v)
       Register
       (register [this f]
         (when-not (dosync (when @q
                             (alter q conj f)))
           (f this)
           this))
-      Supply
-      (supply [this x]
+      ;; Implementation of IFn for deliver:
+      clojure.lang.IFn
+      (invoke [this x]
         (when-let [watchers (dosync (when @q
                                       (ref-set v x)
                                       (let [dq @q]
@@ -34,180 +29,175 @@
           (.countDown latch)
           (doseq [w watchers]
             (w this))
-          this)))))
-
-(comment
-  (defn postpone [ipending f & args]
-    (let [p (active-promise)]
-      (register ipending
-                (fn [that]
-                  (supply p (apply f @that args))))
-      p)))
-
-(defn postpone [source f & args]
-  (let [a (atom nil)]
-    (register source (fn [that]
-                       (reset! a (apply f @that args))))
-    (reify
+          x))
       clojure.lang.IPending
       (isRealized [this]
-        (realized? source))
+        (zero? (.getCount latch)))
       clojure.lang.IDeref
       (deref [this]
-        (deref source) ;; blocks
-        (deref a))
-      Register
-      (register [this f]
-        (register source (fn [_] (f this)))
-        this))))
+        (.await latch)
+        @v)
+      clojure.lang.IBlockingDeref
+      (deref
+        [_ timeout-ms timeout-val]
+        (if (.await latch timeout-ms java.util.concurrent.TimeUnit/MILLISECONDS)
+          @v
+          timeout-val)))))
 
+(defn apply-when-realized
+  "Returns a notifier which will receive the result of 
+  (apply f @r args) when r becomes realized."
+  [r f & args]
+  (let [n (notifier)]
+    (register r (fn [v] (deliver n (apply f @v args))))
+    n))
 
-(declare proseq)
+;; (declare proseq)
 
-(deftype PromisedSeq [latch q v]
-  Supply
-  (supply [this x]
-    (when-let [watchers (dosync (when @q
-                                  (ref-set v x)
-                                  (let [dq @q]
-                                    (ref-set q nil)
-                                    dq)))]
-      (.countDown latch)
-      (doseq [w watchers]
-        (w this)))
-    this)
-  Register
-  (register [this f]
-    (when-not (dosync (when @q
-                        (alter q conj f)))
-      (f this))
-    this)
-  clojure.lang.IPending
-  (isRealized [this]
-    (zero? (.getCount latch)))
-  clojure.lang.Seqable
-  (seq [this]
-    (.await latch)
-    (seq @v))
-  clojure.lang.ISeq
-  (first [this]
-    (first (seq this)))
-  (more [this]
-    (rest (seq this)))
-  (next [this]
-    (seq (rest this)))
-  (count [this]
-    (count (seq this)))
-  (cons [this o]
-    (clojure.lang.Cons. o this))
-  (empty [this]
-    (proseq))
-  (equiv [this that]
-    (and (realized? this)
-         (instance? clojure.lang.ISeq that)
-         (= (seq this) (seq that)))))
+;; (deftype PromisedSeq [latch q v]
+;;   clojure.lang.IFn
+;;   (invoke [this x]
+;;     (when-let [watchers (dosync (when @q
+;;                                   (ref-set v x)
+;;                                   (let [dq @q]
+;;                                     (ref-set q nil)
+;;                                     dq)))]
+;;       (.countDown latch)
+;;       (doseq [w watchers]
+;;         (w this)))
+;;     x)
+;;   Register
+;;   (register [this f]
+;;     (when-not (dosync (when @q
+;;                         (alter q conj f)))
+;;       (f this))
+;;     this)
+;;   clojure.lang.IPending
+;;   (isRealized [this]
+;;     (zero? (.getCount latch)))
+;;   clojure.lang.Seqable
+;;   (seq [this]
+;;     (.await latch)
+;;     (seq @v))
+;;   clojure.lang.ISeq
+;;   (first [this]
+;;     (first (seq this)))
+;;   (more [this]
+;;     (rest (seq this)))
+;;   (next [this]
+;;     (seq (rest this)))
+;;   (count [this]
+;;     (count (seq this)))
+;;   (cons [this o]
+;;     (clojure.lang.Cons. o this))
+;;   (empty [this]
+;;     (proseq))
+;;   (equiv [this that]
+;;     (and (realized? this)
+;;          (instance? clojure.lang.ISeq that)
+;;          (= (seq this) (seq that)))))
 
-(defn proseq
-  "Returns a proseq (promised seq). Call supply to provide it with
-  an ISeq."
-  []
-  (let [latch (java.util.concurrent.CountDownLatch. 1)
-        q (ref [])
-        v (ref nil)]
-    (PromisedSeq. latch q v)))
+;; (defn proseq
+;;   "Returns a proseq (promised seq). Call supply to provide it with
+;;   an ISeq."
+;;   []
+;;   (let [latch (java.util.concurrent.CountDownLatch. 1)
+;;         q (ref [])
+;;         v (ref nil)]
+;;     (PromisedSeq. latch q v)))
 
-(defmethod clojure.core/print-method PromisedSeq [x writer]
-  (.write writer (str "#<PromisedSeq "
-                      (if (realized? x)
-                        (first x)
-                        :pending)
-                      ">")))
+;; (defmethod clojure.core/print-method PromisedSeq [x writer]
+;;   (.write writer (str "#<PromisedSeq "
+;;                       (if (realized? x)
+;;                         (first x)
+;;                         :pending)
+;;                       ">")))
+;; (comment
+;;  (deftype Pump [current]
+;;    Register
+;;    (register [this f]
+;;      (register @current f))
+;;    Supply
+;;    (supply [this x]
+;;      (if (nil? x)
+;;        (supply @current nil)
+;;        (do
+;;          (supply @current (cons x (proseq)))
+;;          (swap! current
+;;                 (fn [state]
+;;                   ;; Skip realized elements until we reach nil or pending.
+;;                   (if (and state (realized? state))
+;;                     (recur (rest state))
+;;                     state)))))
+;;      this)
+;;    clojure.lang.IDeref
+;;    (deref [this]
+;;      @current)))
 
-(deftype Pump [current]
-  Register
-  (register [this f]
-    (register @current f))
-  Supply
-  (supply [this x]
-    (if (nil? x)
-      (supply @current nil)
-      (do
-        (supply @current (cons x (proseq)))
-        (swap! current
-               (fn [state]
-                 ;; Skip realized elements until we reach nil or pending.
-                 (if (and state (realized? state))
-                   (recur (rest state))
-                   state)))))
-    this)
-  clojure.lang.IDeref
-  (deref [this]
-    @current))
+;; (defn pump
+;;   ([]
+;;      (pump (proseq)))
+;;   ([s]
+;;      {:pre [(instance? PromisedSeq s)]}
+;;      (Pump. (atom s))))
 
-(defn pump
-  ([]
-     (pump (proseq)))
-  ([s]
-     {:pre [(instance? PromisedSeq s)]}
-     (Pump. (atom s))))
+;; (defmethod clojure.core/print-method Pump [x writer]
+;;   (.write writer "#<Pump>"))
 
-(defmethod clojure.core/print-method Pump [x writer]
-  (.write writer "#<Pump>"))
+;; ;; SiphonedSeq does not implement Supply, so you can't inject values
+;; ;; into it.
+;; (deftype SiphonedSeq [p]
+;;   Register
+;;   (register [this that] (register p that))
+;;   clojure.lang.IPending
+;;   (isRealized [this] (realized? p))
+;;   clojure.lang.Seqable
+;;   (seq [this] (seq p))
+;;   clojure.lang.ISeq
+;;   (first [this] (first p))
+;;   (more [this] (rest p))
+;;   (next [this] (next p))
+;;   (count [this] (count p))
+;;   (cons [this that] (cons p that))
+;;   (empty [this] (empty p))
+;;   (equiv [this that] (.equiv p that)))
 
-;; SiphonedSeq does not implement Supply, so you can't inject values
-;; into it.
-(deftype SiphonedSeq [p]
-  Register
-  (register [this that] (register p that))
-  clojure.lang.IPending
-  (isRealized [this] (realized? p))
-  clojure.lang.Seqable
-  (seq [this] (seq p))
-  clojure.lang.ISeq
-  (first [this] (first p))
-  (more [this] (rest p))
-  (next [this] (next p))
-  (count [this] (count p))
-  (cons [this that] (cons p that))
-  (empty [this] (empty p))
-  (equiv [this that] (.equiv p that)))
+;; (defn siphon* [f s]
+;;   (if (realized? s)
+;;     (f s))
+;;   (let [p (proseq)]
+;;     (register s (fn [that]
+;;                   (supply p (f that))))
+;;     (SiphonedSeq. p)))
 
-(defn siphon* [f s]
-  (if (realized? s)
-    (f s))
-  (let [p (proseq)]
-    (register s (fn [that]
-                  (supply p (f that))))
-    (SiphonedSeq. p)))
+;; (defmacro siphon [bindings & body]
+;;   {:pre [(vector? bindings) (= 2 (count bindings))]}
+;;   `(siphon* (fn [~(first bindings)] ~@body) ~(second bindings)))
 
-(defmacro siphon [bindings & body]
-  {:pre [(vector? bindings) (= 2 (count bindings))]}
-  `(siphon* (fn [~(first bindings)] ~@body) ~(second bindings)))
+;; (defn a-map [f ps]
+;;   (siphon [s ps]
+;;     (when-let [c (seq s)]
+;;       (cons (f (first c))
+;;             (a-map f (rest c))))))
 
-(defn a-map [f ps]
-  (siphon [s ps]
-    (when-let [c (seq s)]
-      (cons (f (first c))
-            (a-map f (rest c))))))
+;; (defn a-filter [f ps]
+;;   (siphon [s ps]
+;;     (when-let [c (seq s)]
+;;       (if (f (first c))
+;;         (cons (first c)
+;;               (a-filter f (rest c)))
+;;         (a-filter f (rest c))))))
 
-(defn a-filter [f ps]
-  (siphon [s ps]
-    (when-let [c (seq s)]
-      (if (f (first c))
-        (cons (first c)
-              (a-filter f (rest c)))
-        (a-filter f (rest c))))))
+;; (defn a-take [n ps]
+;;   (siphon [s ps]
+;;     (when-let [c (seq s)]
+;;       (when (pos? n)
+;;         (cons (first c) (a-take (dec n) (rest ps)))))))
 
-(defn a-take [n ps]
-  (siphon [s ps]
-    (when-let [c (seq s)]
-      (when (pos? n)
-        (cons (first c) (a-take (dec n) (rest ps)))))))
+;; ;;; Maybe the queue should contain WeakReferences. Then if the result
+;; ;;; sequence is not used, the notification never happens.
 
-;;; Maybe the queue should contain WeakReferences. Then if the result
-;;; sequence is not used, the notification never happens.
-
-;; Local Variables:
-;; mode: clojure
-;; eval: (define-clojure-indent (siphon (quote defun)))
-;; End:
+;; ;; Local Variables:
+;; ;; mode: clojure
+;; ;; eval: (define-clojure-indent (siphon (quote defun)))
+;; ;; End:
