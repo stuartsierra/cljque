7d51cce4a1578b8f0a81e2fcf1de3f0331184363
Stuart Sierra <mail@stuartsierra.com>
2012-07-08 16:03:50 -0400

    Change to Leiningen structure

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject cljque "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :repl-options {:init-ns cljque.inotify})
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd `dirname $0`
-
-exec java -cp "src/main/clojure:target/dependency/*" \
-    clojure.main -i "src/main/clojure/cljque/inotify.clj" \
-    -e "(in-ns 'cljque.inotify)" -r
