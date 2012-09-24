58e6b7045eadd6206b3537f7b522c1c894368b02
Stuart Sierra <mail@stuartsierra.com>
2012-09-24 15:13:26 -0500

    Add 'dumbrepl' alias to Leiningen project

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject cljque "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :repl-options {:init-ns cljque.inotify})
+  :repl-options {:init-ns cljque.inotify}
+  :aliases {"dumbrepl" ["trampoline" "run" "-m" "clojure.main"]})
