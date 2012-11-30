f824de0fce024bdf14e454daf3c45767cd184dea
Stuart Sierra <mail@stuartsierra.com>
2012-11-30 16:53:29 -0500

    [WIP] First draft reducible promises; needs Clojure 1.5

--- dev/user.clj
+++ dev/user.clj
@@ -2,7 +2,8 @@
   (:refer-clojure :exclude (promise deliver future future-call))
   (:use clojure.repl
         clojure.tools.namespace.repl
-        cljque.promises))
+        cljque.promises)
+  (:require [clojure.core.reducers :as r]))
 
 (defn thread []
   (.. Thread currentThread getName))
@@ -20,3 +21,54 @@
       (then v (prn :on (thread) :v v) (* v 10))))
 
 
+(def a (promise))
+(def b (promise))
+(def c (promise))
+(def d (promise))
+(def e (promise))
+
+(def all-3 (all a b c))
+
+(def any-1 (any a b c d e))
+
+(def all-3-realized (all-realized a b c))
+
+(defn promise-reduce* [return executor f init input]
+  (attend input
+          (fn [p]
+            (try (if @p
+                   (let [[x next-input] @p
+                         next-init (f init x)]
+                     (if (reduced? next-init)
+                       (deliver return @next-init)
+                       (promise-reduce* return executor
+                                        f next-init next-input)))
+                   (deliver return init))
+                 (catch Throwable t
+                   (fail return t))))
+          executor))
+
+(defn promise-reduce [f init pcoll]
+  (let [return (promise)]
+    (promise-reduce* return *callback-executor*
+                     f init pcoll)
+    return))
+
+(defn push [promise value]
+  (let [p (promise)]
+    (if (deliver promise [value p])
+      p
+      (if @promise
+        (let [[_ next-promise] @promise]
+          (push next-promise value))
+        (throw (ex-info
+                "Cannot push to a realized promise with a nil value"
+                {:promise promise
+                 :value value}))))))
+
+(extend-protocol clojure.core.protocols/CollReduce
+  cljque.promises.Promise
+  (coll-reduce [coll f]
+    (throw (ex-info "Not implemented" {})))
+  (coll-reduce [coll f val]
+    (promise-reduce f val coll)))
--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject cljque "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]
+                 [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]]
+  :repositories {"sonatype-oss-public"
+                 "https://oss.sonatype.org/content/groups/public/"}
   :aliases {"dumbrepl" ["trampoline" "run" "-m" "clojure.main"]}
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.1"]
