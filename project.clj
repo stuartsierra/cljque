304109b4d8250cd8dd63bb640dd11525c5b21de6
Stuart Sierra <mail@stuartsierra.com>
2013-03-19 11:24:30 -0700

    Use tools.namespace 0.2.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
                  "https://oss.sonatype.org/content/groups/public/"}
   :aliases {"dumbrepl" ["trampoline" "run" "-m" "clojure.main"]}
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.1"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.3-SNAPSHOT"]
                                   [org.clojure/java.classpath "0.2.0"]]}})
